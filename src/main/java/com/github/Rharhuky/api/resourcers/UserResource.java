f8992f48437a0ec4172c3db1fef767585c8f80f0
Rharhuky <rharhuky@gmail.com>
2024-03-29 21:02:38 -0300

    add: testing findById UserResource controller.

--- src/main/java/com/github/Rharhuky/api/resourcers/UserResource.java
+++ src/main/java/com/github/Rharhuky/api/resourcers/UserResource.java
@@ -15,8 +15,8 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    private final UserService userService;
-    private final ModelMapper modelMapper;
+    private UserService userService;
+    private ModelMapper modelMapper;
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Long id){
@@ -29,16 +29,13 @@ public class UserResource {
 
     @PostMapping
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO) {
-
         var uri = ServletUriComponentsBuilder
                 .fromCurrentRequestUri()
                 .path("/{id}")
                 .buildAndExpand(userService.create(userDTO)
                         .getId())
                 .toUri();
-
         return ResponseEntity.created(uri).build();
-
     }
 
     @PutMapping("/{id}")
--- src/test/java/com/github/Rharhuky/api/resourcers/UserResourceTest.java
+++ src/test/java/com/github/Rharhuky/api/resourcers/UserResourceTest.java
@@ -4,12 +4,20 @@ import com.github.Rharhuky.api.domain.User;
 import com.github.Rharhuky.api.domain.dto.UserDTO;
 import com.github.Rharhuky.api.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserResourceTest {
@@ -37,11 +45,26 @@ class UserResourceTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         startUser();
-
     }
 
     @Test
+    @DisplayName(value = "Return Success when FindById")
     void findById() {
+        when(userService.findById(anyLong())).thenReturn(user);
+        when(modelMapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = userResource.findById(ID);
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        var theUser = response.getBody();
+
+        assertEquals(ID, theUser.getId());
+        assertEquals(NAME, theUser.getName());
+        assertEquals(EMAIL, theUser.getEmail());
+
     }
 
     @Test
